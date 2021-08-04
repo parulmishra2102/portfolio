7ea63dc05497a811e26d959caf38dae6835d4374
Shubham Chaturvedi <shubhamchaturvedi@Shubhams-MacBook-Pro.local>
2021-08-04 21:58:00 +0530

    contact page added

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,25 +1,49 @@
-import "./Contact.scss"
-
-
+import './Contact.scss';
+import { useState } from 'react';
 
 function Contact(props) {
-    return (
-        <div className='contact' id="contact">
-           <div className="left">
-              
-                   <img src="assets/shake.svg" alt-=""></img>
-                   </div>
-                   <div className='right'>
-                       <h2>Contact</h2>
-                       <form>
-                           <input type="text" placeholder="Email id"/>
-                           <textarea placeholder="message"></textarea>
-                           <button type="send"></button>
-                           </form>
-               </div>
-           </div>
-      
-    )
+  const [message, setmessage] = useState(false);
+  const [contactData, setContactData] = useState({
+    mail: '',
+    message: ' ',
+  });
+
+  const saveDatahandler = (event) => {
+    var data={...contactData}
+    setContactData({...data,[event.target.name]:event.target.value})
+   
+    console.log(contactData);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setmessage(true);
+  };
+  return (
+    <div className="contact" id="contact">
+      <div className="left">
+        <img src="assets/shake.svg" alt-=""></img>
+      </div>
+      <div className="right">
+        <h2>Contact</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="mail"
+            onChange={saveDatahandler}
+            placeholder="Email id"
+          />
+          <textarea
+            placeholder="message"
+            name="message"
+            onChange={saveDatahandler}
+          ></textarea>
+          <button type="send">Send</button>
+          {message ?  <span> your {contactData.mail} id is registered to us, I'll get back to you soon</span> : ''} 
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
